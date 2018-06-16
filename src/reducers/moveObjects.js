6606f715ba147670ecb784285be0a70b798a500b
Kaji Tetsushi <kajitetsushi@outlook.com>
2018-06-16 19:39:11 +0800

    feat(flyingobject): add CSS-based downwards movement

--- src/reducers/moveObjects.js
+++ src/reducers/moveObjects.js
@@ -1,4 +1,6 @@
+import { flyingObjectLifespan } from '../utils/constants';
 import { calculateAngle } from '../utils/formulas';
+import { getCurrentTime } from '../utils/helpers';
 import createFlyingObjects from './createFlyingObjects';
 
 const moveObjects = (state, action) => {
@@ -11,11 +13,21 @@ const moveObjects = (state, action) => {
   // Create flying objects if needed.
   const newState = createFlyingObjects(state);
 
+  // Destroy flying objects which have lived for longer than 4s in the game.
+  const now = getCurrentTime();
+  const flyingObjects = newState.gameState.flyingObjects.filter(flyingObject => (
+    (now - flyingObject.createdAt) < flyingObjectLifespan
+  ));
+
   const { x, y } = mousePosition;
   const angle = calculateAngle(0, 0, x, y);
 
   return {
     ...newState,
+    gameState: {
+      ...newState.gameState,
+      flyingObjects,
+    },
     angle,
   };
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,6 +3,7 @@ export const skyAndGroundWidth = 5000;
 
 // start button width
 export const gameWidth = 800;
+export const gameHeight = 1200;
 
 // new flying objects every interval duration in milliseconds
 export const createInterval = 1000;
@@ -10,8 +11,10 @@ export const createInterval = 1000;
 // no more than this number of objects on screen
 export const maxFlyingObjects = 4;
 
-// flying objects start at this "altitude" in the Y axis
-export const flyingObjectsStarterYAxis = -1000;
+// flying objects start at this "altitude" in the Y axis.
+// ideally, spawn in a place above the browser viewport 
+// so that the players don't see the flying objects pop out of nowhere.
+export const flyingObjectsStarterYAxis = -1100;
 
 // possible starting X-axis positions the flying objects will spawn in
 export const flyingObjectsStarterPositions = [
@@ -20,3 +23,5 @@ export const flyingObjectsStarterPositions = [
   150,
   300,
 ];
+
+export const flyingObjectLifespan = 4000;
